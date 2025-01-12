3b7970f2caabcec661e7145e2ce4c4a1c0254fbf
Antonio <tonilocke@hotmail.it>
2025-01-12 12:01:52 +0100

    fixes, working on keyboard input

--- src/components/DrumMachine.tsx
+++ src/components/DrumMachine.tsx
@@ -1,21 +1,59 @@
-import { useState } from "react";
-import DrumPad from "./DrumPad";
+import { useEffect, useState } from "react";
+//import DrumPad from "./DrumPad";
 const DrumMachine = ()=>{
-    const [description, setDescription] = useState("");
+    const [description, setDescription] = useState<string>("");
+    const [keyp, setKeyp] = useState<string>("");
+    function handleKeyDown(e:any){
+        const keydown = e.key.toUpperCase();
+        setKeyp(keydown);
+        switch(keydown){
+            case "Q":
+                console.log(keydown);
+                break;
+            case "W":
+                console.log(keydown);
+                break;
+            case "E":
+                console.log(keydown);
+                break;
+            case "A":
+                console.log(keydown);
+                break;
+            case "S":
+                console.log(keydown);
+                break;
+            case "D":
+                console.log(keydown);
+                break;
+            case "Z":
+                console.log(keydown);
+                break;
+            case "X":
+                console.log(keydown);
+                break;
+            case "C":
+                console.log(keydown);
+                break;                
+        }
+    }
+    useEffect(()=>{
+        
+    },[keyp]);
     return(
-        <main id="drum-machine">
-            <section id="display">
-                <DrumPad  idText= "Clap"      letter={"Q"} setDescription = { setDescription }></DrumPad>
-                <DrumPad  idText= "Closed_HH" letter={"W"} setDescription = { setDescription }></DrumPad>
-                <DrumPad  idText= "Heater-1"  letter={"E"} setDescription = { setDescription }></DrumPad>
-                <DrumPad  idText= "Heater-2"  letter={"A"} setDescription = { setDescription }></DrumPad>
-                <DrumPad  idText= "Heater-3"  letter={"S"} setDescription = { setDescription }></DrumPad>
-                <DrumPad  idText= "Heater-4"  letter={"D"} setDescription = { setDescription }></DrumPad>
-                <DrumPad  idText= "Kick_n_Hat"letter={"Z"} setDescription = { setDescription }></DrumPad>
-                <DrumPad  idText= "Kick"      letter={"X"} setDescription = { setDescription }></DrumPad>
-                <DrumPad  idText= "Open_HH"   letter={"C"} setDescription = { setDescription }></DrumPad>
+        <main id="drum-machine" onKeyDown={(e)=>handleKeyDown(e)}>
+           {/* <section id="display">
+                <DrumPad  idText= "Clap"      letter={ "Q" }  setDescription = { setDescription }></DrumPad>
+                <DrumPad  idText= "Closed_HH" letter={ "W" }  setDescription = { setDescription }></DrumPad>
+                <DrumPad  idText= "Heater-1"  letter={ "E" }  setDescription = { setDescription }></DrumPad>
+                <DrumPad  idText= "Heater-2"  letter={ "A" }  setDescription = { setDescription }></DrumPad>
+                <DrumPad  idText= "Heater-3"  letter={ "S" }  setDescription = { setDescription }></DrumPad>
+                <DrumPad  idText= "Heater-4"  letter={ "D" }  setDescription = { setDescription }></DrumPad>
+                <DrumPad  idText= "Kick_n_Hat"letter={ "Z" }  setDescription = { setDescription }></DrumPad>
+                <DrumPad  idText= "Kick"      letter={ "X" }  setDescription = { setDescription }></DrumPad>
+                <DrumPad  idText= "Open_HH"   letter={ "C" }  setDescription = { setDescription }></DrumPad>
                 { description }
-            </section>
+            </section>*/}
+            { keyp }
         </main>
     );
 }
--- src/components/DrumPad.tsx
+++ src/components/DrumPad.tsx
@@ -1,11 +1,11 @@
-import { useState,useRef } from "react";
+import { useState, useRef, KeyboardEvent  } from "react";
 type Props = {
     idText:string,
     letter:string,
     setDescription: Function
 }
 
-function DrumPad({ idText, letter,  setDescription}: Props){
+function DrumPad({ idText, letter, setDescription }: Props){
     const [playing, setPlaying] = useState<boolean>(false);
     const ref = useRef(null);
     function playSound(){
@@ -18,8 +18,9 @@ function DrumPad({ idText, letter,  setDescription}: Props){
             ref.current.pause();
         }
     }
+    
     return(
-        <section id={ idText } className="drum-pad">
+        <section id={ idText } className="drum-pad"  >
             <button onClick={()=>playSound()}>{ letter }</button>
             <audio src={`src/assets/${idText}.mp3`} className="clip" id={ letter } onEnded={()=>setPlaying(false)} onPlay={()=>setPlaying(true)} ref ={ ref } ></audio>
         </section>
